43138ce655470fda6a77707d3e76cb11e06aa974
Sameeh <sameehkhan1@gmail.com>
2019-02-19 09:39:59 -0800

    Updates to json

--- assets/js/boards/bundle.js
+++ assets/js/boards/bundle.js
@@ -2366,49 +2366,10 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 document.addEventListener('DOMContentLoaded', () => {
     console.dir(__WEBPACK_IMPORTED_MODULE_0_nba___default.a)
-    // let lebron = NBA.playerIdFromName('Lebron James');
-    // let kings = NBA.teamIdFromName('Sacramento Kings');
-    // console.log(lebron)
-    // NBA.stats.playerInfo({ PlayerID: lebron }).then(console.log);
-    // NBA.stats.teamSplits({TeamID: kings}).then(console.log);
-    // NBA.stats.teamYears({TeamID: kings}).then(console.log);
     //colorbrewer.org 
     Object(__WEBPACK_IMPORTED_MODULE_1__barchart__["a" /* default */])();
     Object(__WEBPACK_IMPORTED_MODULE_2__scatterplot2__["a" /* default */])();
     // scatterplot();
-
-
-    // console.log(Object.getOwnPropertyNames(NBA));
-    // console.log(NBA.stats.all);
-    // console.log(NBA.prototypes)
-
-    // console.log(Object.getOwnPropertyNames(NBA).filter(function (p) {
-    //     return typeof NBA[p] === 'function';
-    // }));
-
-    // let lebron = NBA.playerIdFromName('Lebron James');
-    // NBA.stats.playerInfo({ PlayerID: lebron }).then(console.log);
-
-
-
-
-    // NBA.stats.teamStats().then(console.log)
-    // Scatterplot with wRank and fg3mRank
-    
-    // NBA.stats.teamSplits({TeamID: 1610612744}).then(console.log)
-    // Warriors team splits maybe do others as well? dropdown?
-
-    // NBA.stats.playerSplits({PlayerID: lebron}).then(console.log)
-
-
-    // NBA.stats.teamStats().then(console.log) // doesn't work
-    // NBA.stats.playerStats().then(console.log) // doesn't work
-
-    // console.dir(NBA.stats.teamStats())
-    // console.dir(NBA)
-    // console.dir(NBA.sportVu)
-
-    // NBA.sportVu.touches().then(console.log)
 })  
 
 
