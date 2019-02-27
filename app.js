2b253c81c375c3d9f597da7c63218d7f159e6169
Sameeh <sameehkhan1@gmail.com>
2019-02-27 15:11:15 -0800

    Update routes for app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require('path')
 const fetch = require('node-fetch')
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
 
-app.use(express.static(path.join(__dirname, 'assets')))
-app.use(express.static(path.join(__dirname, 'images')))
-app.use(express.static(path.join(__dirname, 'tools')))
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'tools')));
 
 app.get('/', (request, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
